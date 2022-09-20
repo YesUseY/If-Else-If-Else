17f1d30dff751a7de7022d55b8ba10546424be03
Lukas <l.lenktaitis123@gmail.com>
2022-09-20 16:23:01 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    cin >> a;
+    if (a>=18) {
+
+        cout << "Asmuo yra pilnametis";
+
+    } else if (a>=0) {
+
+    cout << "Asmuo nera pilnametis";
+
+    } else {
+
+    cout << "Ivestas nelogiskas skaicius";
+
+    }
+    return 0;
+}
